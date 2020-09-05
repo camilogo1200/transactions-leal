90e00be47c6ae450974dbe41c8bd401fbcedfcb3
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2020-09-05 13:29:02 -0500

    [LEAL-T000] - Project base ( initial commit )

---
+++ app/src/main/java/com/leal/camilogo1200/androidtest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.leal.camilogo1200.androidtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
