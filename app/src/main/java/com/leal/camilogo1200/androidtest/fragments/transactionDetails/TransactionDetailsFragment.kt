6b2c98319524d01a99fa67162b7318f87e1474ce
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2020-09-08 19:07:12 -0500

    [LEAL-T002] - Gradle Infrastructure Libraries Updated [LEAL-T002] - Navigation graph updated [LEAL-T002] - Added animations for fragment changes [LEAL-T002] - Added Splash Screen [LEAL-T002] - Added DataBinding [LEAL-T002] - Added App icon [LEAL-T002] - Added menu for  main transaction screen [LEAL-T002] - Added color scheme based on material design [LEAL-T002] - changed the parent to material bridge theme [LEAL-T002] -  added personalization for SnackBar

---
+++ app/src/main/java/com/leal/camilogo1200/androidtest/fragments/transactionDetails/TransactionDetailsFragment.kt
@@ -0,0 +1,19 @@
+package com.leal.camilogo1200.androidtest.fragments.transactionDetails
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.leal.camilogo1200.androidtest.R
+
+class TransactionDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transaction_details, container, false)
+    }
+}
