713c413c7da9458a337394ae3a548f9e1a828617
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2020-09-09 00:37:18 -0500

    [LEAL-T003] - Added animation for drawable transitions [LEAL-T003] - Added animations for fromright & toRight animations on fragment transitions [LEAL-T003] - Added animation to splashScreen [LEAL-T003] - Added Implementation for coroutines on android

--- app/src/main/java/com/leal/camilogo1200/androidtest/fragments/SplashScreenFragment.kt
+++ app/src/main/java/com/leal/camilogo1200/androidtest/fragments/SplashScreenFragment.kt
@@ -1,20 +1,46 @@
 package com.leal.camilogo1200.androidtest.fragments
 
+import android.graphics.drawable.TransitionDrawable
+import android.opengl.Visibility
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.leal.camilogo1200.androidtest.R
+import com.leal.camilogo1200.androidtest.databinding.FragmentSplashScreenBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class SplashScreenFragment : Fragment() {
 
+    private lateinit var _binding: FragmentSplashScreenBinding
+    private lateinit var alphaFadeInAnimation: Animation
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        alphaFadeInAnimation =
+            AnimationUtils.loadAnimation(requireActivity(), R.anim.splash_anim_01)
+        lifecycleScope.launch(Dispatchers.Main) {
+            val transition: TransitionDrawable = _binding.root.background as TransitionDrawable
+            transition.startTransition(2000)
+            _binding.logoLealSplashScreen.animation = alphaFadeInAnimation
+            _binding.mainTransactionsTitleText.animation = alphaFadeInAnimation
+            delay(2000)
+            _binding.logoLealSplashScreen.visibility = View.VISIBLE
+            findNavController().navigate(R.id.action_splashScreenFragment_to_transactionsListFragment)
+        }
+        return _binding.root
+
     }
 
 }
